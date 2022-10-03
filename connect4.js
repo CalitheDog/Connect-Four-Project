e8ed40cc6a236dc13197eac6f02a3e0e0b481669
Jacob Valdez <47723998+CalitheDog@users.noreply.github.com>
2022-10-02 19:37:30 -0500

    Added checkforWin(), findSpotForCol(), tie finder, player switcher, comments.

--- connect4.js
+++ connect4.js
@@ -5,11 +5,11 @@
  * board fills (tie)
  */
 
-let width = 7;
-let height = 6;
+const width = 7;
+const height = 6;
 
 let currPlayer = 1; // active player: 1 or 2
-let board = []; // array of rows, each row is array of cells  (board[y][x])
+const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -25,22 +25,20 @@ board.push(Array.from({length : width}))
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-const htmlBoard = document.getElementById('board')
-  // Creates new chip? Not to sure, will come back to.
-  let top = document.createElement("tr");
-  top.setAttribute("id", "column-top");
+const htmlBoard = document.getElementById('board') 
+let top = document.createElement("tr");
+top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
-
-  for (let i = 0; i < width; i++) {
+  //  Makes the clickable top part
+  for (let x = 0; x < width; x++) {
     let headCell = document.createElement("td");
-    headCell.setAttribute("id", i);
+    headCell.setAttribute("id", x);
     top.append(headCell);
   }
   htmlBoard.append(top);
 
-  // TODO: add comment for this code
-  for (let i = 0; i < height; i++) {
+  // This will make the main part of the board
+  for (let y = 0; y < height; y++) {
     const row = document.createElement("tr");
     for (let x = 0; x < width; x++) {
       const cell = document.createElement("td");
@@ -54,36 +52,45 @@ const htmlBoard = document.getElementById('board')
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
-  return 0;
+for(let y = height -1; y>= 0; y--){
+  if(!board[y][x]){
+    return y
+  }
+}
+return null
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
+  const piece = document.createElement('div');
+  piece.classList.add('piece');
+  piece.classList.add(`p${currPlayer}`)
+  const location = document.getElementById(`${y}-${x}`)
+  location.append(piece)
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+  alert(msg)
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  let x = +evt.target.id;
+  const x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  let y = findSpotForCol(x);
+  const y = findSpotForCol(x);
   if (y === null) {
     return;
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
+board[y][x] = currPlayer;
   placeInTable(y, x);
 
   // check for win
@@ -92,10 +99,13 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
+  if(board.every(row => row.every(cell => cell))){
+    return endGame('Tie')
+  }
+
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+ currPlayer = currPlayer === 1 ? 2 : 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -116,15 +126,16 @@ function checkForWin() {
     );
   }
 
-  // TODO: read and understand this code. Add comments to help you.
+ 
 
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
+      // Defines the directions, and ways to win.
       const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
       const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
       const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
       const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
-
+// Finds the winner with different directions
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
       }
